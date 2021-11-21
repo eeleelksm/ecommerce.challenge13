fbccbb49b2f4b20b27863aae3e33b4be7f905b6f
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-21 11:27:53 -0500

    product_tag columns completed

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
-
+const { Model, DataTypes, DatabaseError } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
+  { // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
