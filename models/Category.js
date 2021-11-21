7257fabb59b8ef770fc8f758e57a2d7ba4f6c996
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-21 11:22:35 -0500

    product columns completed

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
 Category.init(
