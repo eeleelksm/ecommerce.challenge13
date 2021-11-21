00219cba1beb21e131a8031f29713ca4e20fd7ae
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-21 11:24:18 -0500

    tag columns complete

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,20 @@
-const { Model, DataTypes } = require('sequelize');
-
+const { Model, DataTypes, DatabaseError } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  { // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
